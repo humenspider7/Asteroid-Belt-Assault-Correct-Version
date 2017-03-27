b78451120558d686d5f5e2d2136c40f661f9d04e
humenspider7 <iamyusuf6@aol.com>
2017-03-27 12:13:19 -0400

    Early Edits

--- Asteroid Belt Assault/Asteroid Belt Assault/Asteroid Belt Assault/Game1.cs
+++ Asteroid Belt Assault/Asteroid Belt Assault/Asteroid Belt Assault/Game1.cs
@@ -94,8 +94,8 @@ namespace Asteroid_Belt_Assault
 
             playerManager = new PlayerManager(
                 spriteSheet,    
-                new Rectangle(0, 150, 50, 50),    
-                3,
+                new Rectangle(170, 265, 50, 85),    
+                1,
                 new Rectangle(
                     0,
                     0,
